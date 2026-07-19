4c41d28833c581711e6935c6f910c7b54dbc0049
chirag jain <jchirag0610@gmail.com>
2026-07-19 13:03:33 +0530

    branding left section added

---
+++ src/components/Logo.jsx
@@ -0,0 +1,18 @@
+import { ShoppingBag } from "lucide-react"
+
+const Logo = () => {
+    return (
+        <div className="flex items-center gap-3">
+            <div className="flex h-12 w-12 items-center justify-center rounded-2xl bg-lime-400">
+                <ShoppingBag
+                    size={24}
+                    className="text-black" />
+            </div>
+            <h1 className="text-3xl font-bold tracking-tight">
+                Sky<span className="text-lime-400">Mart</span>
+            </h1>
+        </div>
+    )
+}
+
+export default Logo
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,61 @@
+import Logo from "../components/Logo"
+import StatCard from "../components/StatCard";
+
 const Login = () => {
-  return (
-    <main className="min-h-screen bg-neutral-950 text-white">
-        <div className="grid min-h-screen lg:grid-cols-2">
-            <section className="section-left border border-neutral-800 flex flex-col justify-between p-8 lg:p-12">
-                Left Section
-            </section>
-            <section className="section-right border border-neutral-800 flex items-center justify-center p-6 lg:p-12">
-                Right Section
-            </section>
-        </div>
-    </main>
-  )
+    const statistics = [
+        {
+            id: 1,
+            value: "20K+",
+            label: "Products",
+        },
+        {
+            id: 2,
+            value: "50K+",
+            label: "Users",
+        },
+        {
+            id: 3,
+            value: "4.9",
+            label: "Rating",
+            rating: true,
+        },
+    ];
+    return (
+        <main className="min-h-screen bg-neutral-950 text-white">
+            <div className="grid min-h-screen lg:grid-cols-2">
+                <section className="section-left border border-neutral-800 flex flex-col justify-between p-8 lg:p-12">
+                    <Logo />
+                    <div className="max-w-xl">
+                        <p className="mb-6 text-sm font-semibold uppercase text-lime-400 tracking-[0.15em]">
+                            Welcome Back
+                        </p>
+                        <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl">Shop the future.
+                            <br />
+                            <span className="text-lime-400">
+                                Today.
+                            </span>
+                        </h1>
+                        <p className="mt-8 max-w-lg text-lg text-neutral-400 leading-8 mb-4">
+                            Thousands of products, lightning-fast delivery,
+                            and prices that make your wallet happy.
+                        </p>
+                    </div>
+                    <div className="grid grid-cols-3 gap-4">
+                        {statistics.map((item) => {
+                            return <StatCard
+                                key={item.id}
+                                value={item.value}
+                                label={item.label}
+                                rating={item.rating} />
+                        })}
+                    </div>
+                </section>
+                <section className="section-right border border-neutral-800 flex items-center justify-center p-6 lg:p-12">
+                    Right Section
+                </section>
+            </div>
+        </main>
+    )
 }
 
 export default Login
